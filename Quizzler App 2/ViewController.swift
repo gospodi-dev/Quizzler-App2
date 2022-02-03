c27bbae24427fa427a0d6120e0e930e45a2b5c8b
Gospodi <gospodijazzet@gmail.com>
2022-02-03 12:32:56 +0300

    added IBOutlet

--- Quizzler App 2/ViewController.swift
+++ Quizzler App 2/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
