3a60ff8bcf21838fec6422111c82a85c8fd3c97d
Gospodi <gospodijazzet@gmail.com>
2022-02-03 12:38:37 +0300

    Добавлен функционал для выбора ответа и обновление UI

--- Quizzler App 2/ViewController.swift
+++ Quizzler App 2/ViewController.swift
@@ -26,19 +26,45 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        let userAnswer = sender.currentTitle!
         
+        let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
         
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
         
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    
-    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        // Необходимо получить ответы и обновить заголовки кнопок с помощью метода setTitle.
+        let answerChoices = quizBrain.getAnswers()
+        choice1.setTitle(answerChoices[0], for: .normal)
+        choice2.setTitle(answerChoices[1], for: .normal)
+        choice3.setTitle(answerChoices[2], for: .normal)
+        
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        choice1.backgroundColor = UIColor.clear
+        choice2.backgroundColor = UIColor.clear
+        
+        //Third button needs to be reset too.
+        choice3.backgroundColor = UIColor.clear
+        
+    }
 
 }
-
