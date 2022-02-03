629c24c41c49c6da352288940b4daebbaa4e1bff
Gospodi <gospodijazzet@gmail.com>
2022-02-03 12:47:36 +0300

    Добавлена модель выбора вопросов и ответов

--- Quizzler App 2/Model/QuizBrain.swift
+++ Quizzler App 2/Model/QuizBrain.swift
@@ -24,3 +24,41 @@ struct QuizBrain {
         Question(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correctAnswer: "Gorilla"),
         Question(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correctAnswer: "Australia")
     ]
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    //Need a way of fetching the answer choices.
+    func getAnswers() -> [String] {
+        return quiz[questionNumber].answers
+    }
+    
+    func getProgress() -> Float {
+        return Float(questionNumber) / Float(quiz.count)
+    }
+    
+    mutating func getScore() -> Int {
+        return score
+    }
+    
+     mutating func nextQuestion() {
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+    }
+    
+    mutating func checkAnswer(userAnswer: String) -> Bool {
+        //Need to change answer to rightAnswer here.
+        if userAnswer == quiz[questionNumber].rightAnswer {
+            score += 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
